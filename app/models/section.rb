fe2f71703c32ce25d2a65ad0e58e1f283269a73a
david <david@davids-MacBook-Pro-2.local>
2017-05-06 12:29:56 -0400

    Adding bootstrap, restaurant models, and menu

---
+++ app/models/section.rb
@@ -0,0 +1,5 @@
+class Section
+  def self.all
+    %w(Breakfast Espresso Sandwiches)
+  end
+end
---
+++ db/migrate/20170504195032_create_food_items.rb
@@ -0,0 +1,12 @@
+class CreateFoodItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :food_items do |t|
+      t.string :name
+      t.decimal :price
+      t.string :section
+      t.string :remote_image_url
+
+      t.timestamps
+    end
+  end
+end
